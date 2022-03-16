f726bb34064f691b1f04b972dcfa93f43e579cec
agata-oskroba <agatha.oskroba@gmail.com>
2022-03-16 17:25:03 +0100

    Update GrassTile.java
    grasstile update

--- src/main/java/project12/group19/api/ui/GrassTile.java
+++ src/main/java/project12/group19/api/ui/GrassTile.java
@@ -1,19 +1,31 @@
-package project12.group19.api.ui;
+package main.java.project12.group19.api.ui;
+
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
 
+/**
+ * Class to create a single grass tile using Graphics 2D.
+ */
 public class GrassTile {
     private int xLeft;
     private int yTop;
 
+    /**
+     * Constructor of the object instance.
+     * @param x x coordinate of the tile
+     * @param y y coordinate of the tile
+     */
     public GrassTile(int x, int y) {
         xLeft = x;
         yTop = y;
     }
 
+    /**
+     * Method stroking and filling the shape.
+     * @param g2 an instance of the Graphics2D class
+     */
     public void draw(Graphics2D g2) {
-        Rectangle2D tile = new Rectangle(xLeft,yTop,25,25);
-        //g2.draw(tile);
+        Rectangle2D tile = new Rectangle(xLeft,yTop,5,5);
         g2.fill(tile);
     }
 
