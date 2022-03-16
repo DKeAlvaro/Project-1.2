5a2513d67e51b5fc69519642345ef997948a5c8d
agata-oskroba <agatha.oskroba@gmail.com>
2022-03-16 17:26:06 +0100

    Create Target.java
    target shape

---
+++ src/main/java/project12/group19/api/ui/Target.java
@@ -0,0 +1,21 @@
+package main.java.project12.group19.api.ui;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+public class Target {
+    private double xLeft;
+    private double yTop;
+    private double radius;
+
+    public Target(int x, int y, double r){
+        radius = r*12;
+        xLeft = GrassComponent.coorToSwingX(x*12) - r*6;
+        yTop = GrassComponent.coorToSwingY(y*12) - r*6;
+
+    }
+    public void draw(Graphics2D g2) {
+        Ellipse2D.Double target = new Ellipse2D.Double(xLeft,yTop,2*radius,2*radius);
+        g2.fill(target);
+    }
+}
