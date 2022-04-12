ed6a33d0e4bc8afc567fbecfb6b77452ebc01f27
etki <etki@ayte.team>
2022-04-12 11:49:33 +0200

    Added ODESolver and MultiVariableOptimizer interfaces

---
+++ src/main/java/project12/group19/math/ode/ODESolver.java
@@ -0,0 +1,31 @@
+package project12.group19.math.ode;
+
+import java.util.OptionalDouble;
+import java.util.function.DoubleFunction;
+
+/**
+ * This interface describes a numerical solver of ordinary differential
+ * equation, which, given a value of a function, value of variable,
+ * derivative and step, approximates value of that function at variable
+ * incremented by step.
+ */
+@FunctionalInterface
+public interface ODESolver {
+    /**
+     * Returns approximated value of a function for variable set to
+     * {@code position + step}.
+     *
+     * @param value Current function value.
+     * @param position Current variable value.
+     * @param step Defines difference to next variable value to be
+     * approximated.
+     * @param derivative Function that allows to get derivative value at
+     * for specific value of variable. Since derivative isn't guaranteed
+     * to be defined, it returns an {@link OptionalDouble}, which can be
+     * empty (and implementations should account for that).
+     * @return Approximated function value. Since it's possible for
+     * value to be uncomputable, solver is allowed to return empty
+     * {@link OptionalDouble} as well.
+     */
+    OptionalDouble apply(double value, double position, double step, DoubleFunction<OptionalDouble> derivative);
+}
