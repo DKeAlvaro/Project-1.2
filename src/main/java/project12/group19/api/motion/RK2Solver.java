359a5c4a08e9912becfa3ea9e9f837d0c5a47822
annanowo <91745179+annanowo@users.noreply.github.com>
2022-04-19 12:22:23 +0200

    Create RK2Solver.java

---
+++ src/main/java/project12/group19/api/motion/RK2Solver.java
@@ -0,0 +1,8 @@
+package project12.group19.api.motion;
+
+public class RK2Solver implements MotionCalculator{
+    @Override
+    public MotionState calculate(MotionState state, Acceleration acceleration, double deltaT) {
+        return null;
+    }
+}
