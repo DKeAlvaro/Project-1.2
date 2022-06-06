2ab47227c960172a185b04c6521b12f19f548092
etki <etki@ayte.team>
2022-06-06 14:53:08 +0200

    Changed multivariable optimizer signature and added more comments to it

--- src/main/java/project12/group19/math/optimization/MultiVariableOptimizer.java
+++ src/main/java/project12/group19/math/optimization/MultiVariableOptimizer.java
@@ -13,15 +13,29 @@ import java.util.Optional;
  * providing the initial parameters, controlling number of iterations,
  * checking whether optimization is going the correct way, and so on;
  * the goal of optimizer is to only give the next guess based on
- * previous history.
+ * previous history. The optimizer tries to find a root of the function
+ * closest to the given set of parameters.
  */
 public interface MultiVariableOptimizer {
     /**
-     * Performs a single round of optimization.
+     * Performs a single round of optimization. Given a set of
+     * parameters it should return another set of parameters that, when
+     * fed into function, produce a result closer to zero (meaning they
+     * are closer to root) than previous set.
+     *
+     * For example, for finding the local minimum of the surface
+     * function, one would deal with an array of two parameters
+     * {@code ([x, y])} and would return another array of two elements
+     * (x and y again) that, when fed into function, would return a
+     * value smaller than for initially provided set of parameters.
+     *
+     * Implementations are implied to perform single step of
+     * optimization only, providing a better result, but not the final
+     * one - it is the goal of wrapping infrastructure to iterate the
+     * algorithm till the result is good enough.
      *
      * @param parameters List of variable values for a function.
      * @param function Function that is the subject of estimation.
-     * @param goal Target function value.
      * @param history List of previous optimizations.
      * @param precision A proportion at which solver should try to
      * optimize parameters. 1.0 means "normal" optimization (for some
@@ -36,7 +50,6 @@ public interface MultiVariableOptimizer {
     Optional<double[]> optimize(
             MultiParameterOperation function,
             double[] parameters,
-            double goal,
             List<Round> history,
             double precision
     );
