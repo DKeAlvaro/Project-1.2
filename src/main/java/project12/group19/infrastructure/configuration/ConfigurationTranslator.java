0216d8983ad8dba1484f70664edf9e45888d4b86
etki <etki@ayte.team>
2022-06-14 15:50:11 +0200

    Fixed the mandatory player reference in configuration

--- src/main/java/project12/group19/infrastructure/configuration/ConfigurationTranslator.java
+++ src/main/java/project12/group19/infrastructure/configuration/ConfigurationTranslator.java
@@ -91,7 +91,7 @@ public class ConfigurationTranslator {
                         container.getDouble("r", "course.target.radius")
                 ),
                 container.getDouble(List.of("timeScale", "engine.rates.scale"), 1.0),
-                container.getString("player"),
+                container.getString("player", "bot.naive"),
                 lakes,
                 PlanarDimensions.create(
                         container.getDouble("course.width", 50.0),
